70aaefd9a8c109cf62372bcd01f07898605c3d65
Miles Singleton <milessingleton@hotmail.co.uk>
2021-10-19 10:30:53 +0100

    made function getting if lab for current day+time

--- calandarReader.py
+++ calandarReader.py
@@ -1,8 +1,10 @@
-from datetime import time
+from datetime import datetime, time
 from lxml import html
 from numpy import empty
 #import requests
 import pandas as pd
+
+LOCATION = 'CF104'
 
 class lab:
     name: str
@@ -17,15 +19,13 @@ class lab:
     def __str__(self) -> str:
         return 'Module:    ' + self.name + '\n'+ 'Duration:   ' + self.duration +'\n' + 'Location:     ' + self.location
 
-table = open('FSE Intranet - Timetable.html', 'r').read()
-
-
-
-timetable = pd.read_html(table)
+timetable = pd.read_html(open('FSE Intranet - Timetable.html', 'r').read())
 timetable = timetable[0]
+timetable.to_csv('timetable')
 # Store coloumn names as they change each week
 # index 0 of coloumns is not needed 
 coloumnList = timetable.columns[1:5]
+
 
 
 #Reads Line of table and turns all labs in said slot into objects stored in a list
@@ -41,3 +41,35 @@ for item in commaLine:
             listOfLabs.append(lab('CS' + x[0],x[2],x[3]))
         elif len(x) == 3:
             listOfLabs.append(lab('CS' + x[0],x[1],x[2]))
+
+
+def getLabSlot():
+    day = int(datetime.now().strftime('%u'))
+    hour = int(datetime.now().strftime('%H'))
+    
+    #If it's the weekend or it's before 9am and after 6pm
+    if day > 4 or hour > 18 or hour < 9:
+        return False
+
+    commaLine = str(timetable[getColoumnName(day)][getRow(hour)]).split('CS')
+    listOfLabs = []
+    for item in commaLine:
+        x = item.split('  ')
+        x = list(filter(None,x))
+        if not x :
+            del x
+        else:
+            if len(x) == 4:
+                listOfLabs.append(lab('CS' + x[0],x[2],x[3]))
+            elif len(x) == 3:
+                listOfLabs.append(lab('CS' + x[0],x[1],x[2]))
+
+    return listOfLabs
+
+def getColoumnName(num) -> str:
+    return coloumnList[num-1]
+
+def getRow(num) -> int:
+    #9 == 0
+    #8 == 17
+    return num - 9
