b0b0c303b690595a0cca5f28b0431bba3bdb6009
Miles Singleton <milessingleton@hotmail.co.uk>
2022-03-08 09:23:44 +0000

    changed US-proc to ipynb file

--- Usability Study/usability-study-processing.py
+++
@@ -1,9 +0,0 @@
-import numpy
-import json
-import os
-
-
-def get_JSON_File():
-    with open(os.path.dirname(__file__) + '/US-data.json') as json_file:
-        data = json.load(json_file)
-        return data
